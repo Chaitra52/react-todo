094d6bfb6b2e5981bfe2e37fd48c1b9a78d80552
Chaitra52 <chaitragowda522@gmail.com>
2020-11-02 23:32:37 +0530

    initial commit

---
+++ src/components/test.js
@@ -0,0 +1,22 @@
+
+
+// Arrow function
+const A = () => {
+    console.log('Here')
+}
+
+let numbers = [1,2,3,4];
+
+numbers.map((number,index) => {
+    console.log(number+' '+index)
+})
+
+
+const A = (numbers) => {
+
+}
+
+
+for(i=0;i<4;i++){
+    sum = 1+numbers[i];
+}
